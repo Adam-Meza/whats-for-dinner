5c606575ed9d9c2223e7c07908d8aa262ad8017e
Adam Meza <Adammeza95@gmail.com>
2023-02-17 16:34:28 -0500

    dislays favorites properly

--- main.js
+++ main.js
@@ -44,49 +44,71 @@ var favoriteItems = {
     mains:[],
     desserts:[]
 }
-var mainBox = document.querySelector('.main-box')
-var suggestionBox = document.querySelector('.suggestion-box')
-var letsCookBtn = document.querySelector('.lets-cook');
+var mainBox = document.querySelector('.main-box');
+var suggestionBox = document.querySelector('.suggestion-box');
+var favoritesBox = document.querySelector('.favorites-box');
 var suggestionDisplay = document.querySelector('.suggestion-display');
+var favoritesDisplay = document.querySelector('.favorites-display');
 var displayItemInst = document.querySelector('.item');
 var displayIcon = document.querySelector('.cookpot');
-var favoriteBtn = document.querySelector('.add-favorite')
-var viewFavoritesBtn = document.querySelector('.view-favorites')
-var favoritesDisplay = document.querySelector('.favorites-display')
-var favoritesBox = document.querySelector('.favorites-box')
-var mainMenuBtn = document.querySelector('.main-menu')
+
+var letsCookBtn = document.querySelector('.lets-cook');
+var favoriteBtn = document.querySelector('.add-favorite');
+var viewFavoritesBtn = document.querySelector('.view-favorites');
+var mainMenuBtn = document.querySelector('.main-menu');
+
+// add an eventlistner to favorites display
+// make it wait for double click,
+// then it runs remove favorite() adn then render favorites()
+// revmoe favorite () is going to take the event.target.closest('li').remove()
+
 
 letsCookBtn.addEventListener('click', displayItem);
-favoriteBtn.addEventListener('click', favoriteItem)
-viewFavoritesBtn.addEventListener('click', viewFavorites)
-mainMenuBtn.addEventListener('click', navBackToMain)
+favoriteBtn.addEventListener('click', favoriteItem);
+viewFavoritesBtn.addEventListener('click', renderFavorites);
+mainMenuBtn.addEventListener('click', navBackToMain);
+favoritesDisplay.addEventListener('dblclick', function(){
+    removeFavorite()
+    renderFavorites()
+    viewFavorites()
+})
 
-function makeFavoritesString(typeInst) {
-    var itemsString = ''
-    for (i = 0; i < typeInst.length; i++) {
-        itemsString += `${typeInst[i].name}, `
-    }
-    return itemsString.slice(0, -2)
+function renderFavorites(){
+    favoritesDisplay.innerHTML = '';
+    var sides = formatFavorites(favoriteItems.sides);
+    var mains = formatFavorites(favoriteItems.mains);
+    var desserts = formatFavorites(favoriteItems.desserts);
+    favoritesDisplay.innerHTML = `
+        <div><h2>Sides</h2>
+        ${sides}</div>
+        <div><h2>Mains</h2>
+        ${mains}</div>
+        <div><h2>Desserts</h2>
+        ${desserts}</div>
+    `
+    viewFavorites()
 }
 function viewFavorites(){
-    favoritesDisplay.innerHTML = ''
-    var mains = favoriteItems.mains
-    var test = makeFavoritesString(mains)
-    favoritesDisplay.innerHTML = `
-    <ul></ul>
-        <li><h2>Sides</h2></li>
-        <ul></ul>
-        <li><h2>Mains</h2></li>
-        <ul>${test}</ul>
-        <li><h2>Desserts</h2></li>
-        <ul></ul>
-    `
     favoritesBox.classList.remove('hidden');
     mainBox.classList.add('hidden');
     suggestionBox.classList.add('hidden');
     viewFavoritesBtn.classList.add('hidden');
     mainMenuBtn.classList.remove('hidden');
 }
+
+function formatFavorites(typeInst){
+    var HTMLString = ""
+    if (typeInst.length > 0) {
+        for (var i = 0; i < typeInst.length; i++) {
+            var itemName = typeInst[i].name
+            HTMLString += `<li>${itemName}</li>`
+            return HTMLString
+            } 
+        } else {
+            return "Nothing here, yet!"
+    }
+} 
+
 
 function navBackToMain(){
     favoritesBox.classList.add('hidden');
@@ -96,7 +118,6 @@ function navBackToMain(){
     mainMenuBtn.classList.add('hidden')
     displayIcon.classList.remove('hidden');
     suggestionDisplay.classList.add('hidden');
-;
 }
 
 function displayItem() {
